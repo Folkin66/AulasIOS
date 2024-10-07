7e4bfd9ffd69fcbc4a01717ecab01a48c0d49c53
Samuel Lira <rlsamuelhenrique@gmail.com>
2024-10-07 11:54:15 -0300

    a

---
+++ AULA20_JS/exer.js
@@ -0,0 +1,17 @@
+nome = 'Cleberson';
+nota1 = 10*0.4; 
+nota2 = 2*0.6;
+media =(nota1+nota2)
+
+console.log(`Boa tarde ${nome}, você foi reprovado com ${media} pontos!`);
+
+let valor1 = 'A'
+let valor2 = 'B'
+let troca;
+
+troca = valor1
+valor1 = valor2
+valor2 = troca
+
+
+console.log(valor2, valor1);
---
+++ AULA20_JS/main.js
@@ -0,0 +1,6 @@
+nota1 = 10;
+nota2 = 2;
+nota3 = 10;
+media = (nota1+nota2+nota3) /3;
+
+console.log(`A média do aluno é ${media}.`);
