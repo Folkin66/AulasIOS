bf4ff38eaf8db3d629e796ebd1aa58e2cba8ce4c
Eduardo Tonel <duhtonel@gmail.com>
2024-10-08 17:08:46 -0300

    add

---
+++ AULA19_JS/main.js
@@ -0,0 +1,26 @@
+// let nome = prompt('Digite o seu nome')
+// alert(`${nome}, seu boçal!`) 1!
+
+
+// let idade = Number(prompt ('Qual a sua idade?'))
+
+// if (idade >=18) {
+//     idade = true
+//     alert(`Você já pode ser preso! |${idade}|`)
+// } else {
+//     idade = false
+//     alert(`Você ainda chupa chupeta, seu patife! |${idade}|`)
+// } 2!
+
+
+// let nome = prompt('Qual é o seu nome?');
+// let idade = Number(prompt ('Digite sua idade:'));
+// let genero = prompt('Qual é o seu gênero?')
+
+// if (idade >= 18) {
+//     idade = true
+//     alert(`Olá ${nome}, você é do gênero ${genero} e é maior de idade! |${idade}|`)
+// } else {
+//     idade = false
+//     alert(`Olá ${nome}, você é do gênero ${genero} e é menor de idade! |${idade}|`)
+// }
