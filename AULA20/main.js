4b57cbd28595b51ea25ba36e4a8da17e4f17d6f2
Samuel Lira <rlsamuelhenrique@gmail.com>
2024-10-08 11:55:55 -0300

    aa

---
+++ AULA20/main.js
@@ -0,0 +1,56 @@
+// alert('Bem vindo ao meu programa!'); 1
+
+
+// let nome = prompt (`Qual é o seu nome?`);
+// alert(`Olá ${nome}!`);
+
+// let idade = prompt (`Digite a sua idade.`) 2
+
+
+// if (idade >= 18) {
+//     alert(`Você tem ${idade} anos, já pode ser preso, hehe.`)
+// }
+// else {
+//     alert(`Você tem ${idade} anos, ainda é um patife!`)
+// } 3
+
+
+// let nome = prompt('Seu nome é?');
+// let idade = prompt('Qual a sua idade?');
+// let genre = prompt('Masculino ou Feminino?');
+
+// if (idade >= 18){
+//     alert(`${nome}, você é ${genre} e maior de idade.`)
+// }else{
+//     alert(`${nome}, você é ${genre} e menor de idade.`)
+// } 4
+
+
+// let num1 = Number(prompt('Insira um número.'))
+// let num2 = Number(prompt('Insira outro número.'))
+
+// // alert(`A soma deles é ${num1 + num2}.`)
+// alert(`A subtração deles é ${num1 - num2}.`)
+// // alert(`A divisão deles é ${num1 / num2}.`)
+// // alert(`A multiplicação deles é ${num1 * num2}.`) 5
+
+
+// let isAluno = prompt('Você é um aluno?')
+//  isAluno = isAluno.toLowerCase()
+// if (isAluno == 'sim'){
+//     isAluno = true
+//     alert(`Você é um ${isAluno}!`)
+// } else{
+//     isAluno = false
+//     alert(`Você é ${isAluno}...`)
+// } 6
+
+
+
+let alt = prompt('Digite vossa altura:')
+let peso = prompt('Digite seu peso em kg:')
+
+let calc = peso / (alt * alt)
+
+alert(`Seu IMC é ${calc}.`)
+
