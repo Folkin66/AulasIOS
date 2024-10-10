09d473ca32f0c3f89d9ecfc243f76e64c409d955
Eduardo Tonel <duhtonel@gmail.com>
2024-10-10 05:55:45 -0300

    adicionei a ultima atividade.

--- AULA21/Atividades/atv7.js
+++ AULA21/Atividades/atv7.js
@@ -1,3 +1,15 @@
+// GPT
+// Armazenando o nome e sobrenome em uma variável
+let nomeComplet = "João Silva";
+
+// Criando um array a partir da variável
+let nomeArray = nomeComplet.split(" ");
+
+// Apresentando o sobrenome em uma frase
+console.log(`Meu sobrenome é ${nomeArray[1]}.`);
+
+
+// eu
 let nomeCompleto = 'Edurado Tonel'
 let NomESobrenom = ["Eduardo", "Tonel"]
 console.log(NomESobrenom[1] + ' cuzão.');
